cc78d008a88b6559cd22e831efbb78994181583e
Vutlhari <vutlhari@intelleq.co.za>
2022-07-08 20:58:48 +0200

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <h1 className="text-3xl font-bold ">Burnout Self-Test</h1>
+    </div>
+  )
+}
